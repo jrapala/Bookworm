fb339cccad4239f0a1fe5eab77b14233aada331d
Juliette Rapala <jrapala@gmail.com>
2020-11-17 07:40:21 -0600

    fix: clean example data

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -11,36 +11,10 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var moc
 
-    @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
-
     var body: some View {
-        VStack {
-            List {
-                ForEach(students, id: \.id) { student in
-                    Text(student.name ?? "Unknown")
-                }
-            }
-            Button("Add") {
-                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
-                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
-                
-                let chosenFirstName = firstNames.randomElement()
-                let chosenLastName = lastNames.randomElement()
-                
-                let student = Student(context: self.moc)
-                student.id = UUID()
-                student.name = "\(chosenFirstName ?? "") \(chosenLastName ?? "")"
-            }
-        }
+        Text("Hello World")
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
