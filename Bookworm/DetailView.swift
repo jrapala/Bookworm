6417e36384215440aa110526ae2dd162f9d835ca
Juliette Rapala <jrapala@gmail.com>
2020-11-18 20:51:22 -0600

    feat: add delete button to DetailView

--- Bookworm/DetailView.swift
+++ Bookworm/DetailView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 import CoreData
 
 struct DetailView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    @State private var showingDeleteAlert = false
+    
     let book: Book
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
@@ -41,6 +46,24 @@ struct DetailView: View {
             }
         }
         .navigationBarTitle(Text(book.title ?? "Unknown Book"), displayMode: .inline)
+        .alert(isPresented: $showingDeleteAlert) {
+            Alert(title: Text("Delete book"), message: Text("Are you sure?"), primaryButton: .destructive(Text("Delete")) {
+                    self.deleteBook()
+                }, secondaryButton: .cancel()
+            )
+        }
+        .navigationBarItems(trailing: Button(action: {
+            self.showingDeleteAlert = true
+        }) {
+            Image(systemName: "trash")
+        })
+    }
+    
+    func deleteBook() {
+        moc.delete(book)
+        
+        try? self.moc.save()
+        presentationMode.wrappedValue.dismiss()
     }
 }
 
