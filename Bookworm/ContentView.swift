accdae20064eed39d6cfd05cddc8df0cf4068e3b
Juliette Rapala <jrapala@gmail.com>
2020-11-18 20:40:59 -0600

    feat: add delete function

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -30,9 +30,10 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
             .navigationBarTitle("Bookworm")
-            .navigationBarItems(trailing: Button(action: {
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: {
                 self.showingAddScreen.toggle()
             }) {
                 Image(systemName: "plus")
@@ -43,6 +44,16 @@ struct ContentView: View {
             })
             
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            
+            moc.delete(book)
+        }
+        
+        try? moc.save()
     }
 }
 
